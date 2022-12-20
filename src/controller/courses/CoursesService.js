7e668f803dc6c5c963735fce851d49fb40ed16c8
Divya Singh <er.divyasingh1@gmail.com>
2022-12-20 16:17:15 +0530

    Feature : getting learner details by Id for each leraner Id

--- src/controller/courses/CoursesService.js
+++ src/controller/courses/CoursesService.js
@@ -7,6 +7,10 @@ class CoursesService {
     async getCourseDetailsById(courseId){
         let courseDetails = await getCourseById(courseId);
         courseDetails = JSON.parse(courseDetails)
+        let learners = await Promise.all(courseDetails.learners.map(async (learnerId)=>{   
+            return JSON.parse(await getLearnerById(learnerId));  
+        }));
+        courseDetails.learners = learners;
         return courseDetails;
     }
 }
