c3206e6a28793cc607719185bf5658ccbf5022bb
Divya Singh <er.divyasingh1@gmail.com>
2022-12-20 16:14:55 +0530

    Feature get course by id used in service to get course details

--- src/controller/courses/CoursesService.js
+++ src/controller/courses/CoursesService.js
@@ -1,10 +1,13 @@
 const getCourseById = require("../../thirdPartyApis/LepayaHRApis/getCourseById")
+const getLearnerById = require("../../thirdPartyApis/LepayaHRApis/getLearnerById")
+
 class CoursesService {
     constructor() {
     }
     async getCourseDetailsById(courseId){
-      
-
+        let courseDetails = await getCourseById(courseId);
+        courseDetails = JSON.parse(courseDetails)
+        return courseDetails;
     }
 }
 
